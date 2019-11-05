482c71ece1d71e474e70a78177107ec4f3f79650
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2019-11-05 23:17:01 +0800

    Get SSL certificate first.

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -4,6 +4,9 @@ include_recipe './attributes.rb'
 # Kernel Parameters:
 include_recipe './kernel.rb'
 
+# Install Let's Encrypt:
+include_recipe './lego.rb'
+
 # Prerequisites for Building nginx:
 include_recipe './webadm.rb'
 
@@ -12,6 +15,3 @@ include_recipe './build.rb'
 
 # Setup nginx:
 include_recipe './setup.rb'
-
-# Install Let's Encrypt:
-include_recipe './lego.rb'

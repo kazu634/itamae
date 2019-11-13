36c27f24fe8f86e67a06d8abdeef8a66d6130024
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2019-11-13 11:36:24 +0800

    Delete `grafana-front` cookbook.

--- cookbooks/grafana-front/attributes.rb
+++
@@ -1,9 +0,0 @@
-# -------------------------------------------
-# Specifying the default settings:
-# -------------------------------------------
-node.reverse_merge!({
-  'grafana-front' => {
-    'FQDN' => 'grafana.kazu634.com',
-    'production' => true
-  }
-})

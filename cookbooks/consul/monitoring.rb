a4822fe9ef7be68ddbd3aef153f8a07dff61efaf
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2019-11-24 14:21:03 +0800

    Do not deploy the monitoring settings for `consul`

--- cookbooks/consul/monitoring.rb
+++ cookbooks/consul/monitoring.rb
@@ -1,36 +1,4 @@
-package 'nagios-plugins' do
-  action :install
-
-  options '--no-install-recommends'
-end
-
-remote_file '/usr/lib/nagios/plugins/check_file' do
-  owner 'root'
-  group 'root'
-  mode '555'
-
-  notifies :restart, 'service[supervisor]'
-end
-
-# Deploy the check_memory script:
-package 'bc' do
-  action :install
-end
-
-URL = 'https://raw.githubusercontent.com/zwindler/check_mem_ng/master/check_mem_ng.sh'
-TARGET = '/usr/lib/nagios/plugins/check_memory'
-
-execute "wget #{URL} -O #{TARGET}" do
-  not_if "test -e #{TARGET}"
-end
-
-file TARGET do
-  owner 'root'
-  group 'root'
-  mode '755'
-end
-
-%w(disk load ssh swap reboot-required memory).each do |conf|
+%w(reboot-required).each do |conf|
   remote_file "/etc/consul.d/check-#{conf}.json" do
     owner 'root'
     group 'root'

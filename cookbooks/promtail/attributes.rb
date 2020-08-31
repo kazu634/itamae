ee975266e0a2af76cd1c33e5254ab02a5e37ce7c
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2020-09-01 01:32:27 +0900

    Specify the cookbook-specific attiributes.

---
+++ cookbooks/promtail/attributes.rb
@@ -0,0 +1,14 @@
+# -------------------------------------------
+# Specifying the default settings:
+# -------------------------------------------
+node.reverse_merge!({
+  'promtail' => {
+    'url' => 'https://github.com/grafana/loki/releases/download/',
+    'bin' => 'promtail-linux-amd64.zip',
+    'storage' => '/opt/promtail/bin/',
+    'location' => '/usr/local/bin/',
+    'data' => '/var/opt/promtail/',
+    'lokiendpoint' => '192.168.10.118:3100'
+  },
+})
+

c599fccc2b99a58c2db6355892bc24dc9bb6f402
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2020-09-07 23:15:53 +0900

    Deploy `promtail` config & `systemd` config.

--- cookbooks/nginx/setup.rb
+++ cookbooks/nginx/setup.rb
@@ -68,3 +68,26 @@ end
 service 'nginx' do
   action [ :enable, :start ]
 end
+
+# Deploy `promtail` config file:
+HOSTNAME = run_command('uname -n').stdout.chomp
+
+template '/etc/promtail/nginx.yaml' do
+  owner 'root'
+  group 'root'
+  mode '644'
+
+  variables(HOSTNAME: HOSTNAME, LOKIENDPOINT: node['promtail']['lokiendpoint'])
+end
+
+# Deploy the `systemd` configuration:
+remote_file '/lib/systemd/system/promtail-nginx.service' do
+  owner 'root'
+  group 'root'
+  mode '644'
+end
+
+# Service setting:
+service 'promtail-nginx' do
+  action [ :enable, :restart ]
+end

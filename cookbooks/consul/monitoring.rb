3c634b7dc3885db4f1f2ac78be18dc27cadec17d
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2020-09-22 01:38:34 +0900

    Deploy `check_file_ script.

--- cookbooks/consul/monitoring.rb
+++ cookbooks/consul/monitoring.rb
@@ -1,3 +1,9 @@
+remote_file '/usr/local/bin/check_file' do
+  owner 'root'
+  group 'root'
+  mode '755'
+end
+
 %w(reboot-required).each do |conf|
   remote_file "/etc/consul.d/check-#{conf}.json" do
     owner 'root'

c13954bfc943b8f4ea3be16908ac9dcf7e7c5f5c
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2020-09-23 23:39:13 +0900

    Delete `consul-template`.

--- cookbooks/consul-template/default.rb
+++
@@ -1,6 +0,0 @@
-include_recipe './attributes.rb'
-
-include_recipe './prerequisites.rb'
-include_recipe './install.rb'
-
-include_recipe './setup.rb'
--- cookbooks/consul-template/setup.rb
+++
@@ -1,9 +0,0 @@
-# `consul-template`-related paths:
-%w( /etc/consul-template.d ).each do |d|
-  directory d do
-    owner 'root'
-    group 'root'
-
-    mode '755'
-  end
-end

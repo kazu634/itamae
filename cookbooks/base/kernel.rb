47c2f7784adf52a880c1eb575eb2d1e1e4a044ef
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2020-09-26 16:53:26 +0900

    Modify the kernel parameter to adjust the timeout for nf_conntrack.

--- cookbooks/base/kernel.rb
+++ cookbooks/base/kernel.rb
@@ -2,7 +2,8 @@ STORAGE = '/etc/sysctl.d'
 
 [
   "#{STORAGE}/90-vm-swappiness.conf",
-  "#{STORAGE}/90-vfs-cache-pressure.conf"
+  "#{STORAGE}/90-vfs-cache-pressure.conf",
+  "#{STORAGE}/90-conntrack-tcp-timeout-time-wait.conf"
 ].each do |conf|
   remote_file conf do
     owner 'root'

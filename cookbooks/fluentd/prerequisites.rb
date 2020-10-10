4a9e3d62868654c732bb07bf5e6152dcc6dcfabc
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2020-10-10 15:52:09 +0900

    Delete `fluentd` cookbook.

--- cookbooks/fluentd/prerequisites.rb
+++
@@ -1,5 +0,0 @@
-remote_file '/etc/security/limits.d/90-nfile.conf' do
-  owner 'root'
-  group 'root'
-  mode '644'
-end
--- cookbooks/fluentd/processor.rb
+++
@@ -1,7 +0,0 @@
-%w( processor.conf processor_consul.conf ).each do |f|
-  remote_file "/etc/td-agent/conf.d/#{f}" do
-    owner 'root'
-    group 'root'
-    mode '644'
-  end
-end

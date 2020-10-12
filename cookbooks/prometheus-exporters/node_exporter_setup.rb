057b7fa979cb2d3601ef7dd20fcff1368b8d5700
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2020-10-12 23:56:41 +0900

    Create dedicated cookbook for `Prometheus` exporters.

---
+++ cookbooks/prometheus-exporters/node_exporter_setup.rb
@@ -0,0 +1,39 @@
+# Deploy the `supervisord` configuration:
+remote_file '/etc/supervisor/conf.d/node_exporter.conf' do
+  owner 'root'
+  group 'root'
+  mode '644'
+
+  notifies :restart, 'service[supervisor]'
+end
+
+# Deploy `consul` config for `node_exporter`:
+remote_file '/etc/consul.d/service-node_exporter.json' do
+  owner 'root'
+  group 'root'
+  mode '644'
+
+  notifies :restart, 'service[supervisor]'
+end
+
+service 'supervisor' do
+  action :nothing
+end
+
+# Firewall settings here:
+%w( 9100/tcp ).each do |p|
+  execute "ufw allow #{p}" do
+    user 'root'
+
+    not_if "LANG=c ufw status | grep #{p}"
+
+    notifies :run, 'execute[ufw reload-or-enable]'
+  end
+end
+
+execute 'ufw reload-or-enable' do
+  user 'root'
+  command 'LANG=C ufw reload | grep skipping && ufw --force enable || exit 0'
+
+  action :nothing
+end

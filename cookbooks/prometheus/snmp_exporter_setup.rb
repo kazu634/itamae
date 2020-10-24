ace020419f264fae2d27d33ed4982eb8af499811
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2020-10-24 18:54:58 +0900

    Deploy config file and `supervisord` config.

---
+++ cookbooks/prometheus/snmp_exporter_setup.rb
@@ -0,0 +1,18 @@
+# Create Link
+link '/etc/prometheus_exporters.d/snmp.yml' do
+  to "#{node['snmp_exporter']['storage']}snmp.yml"
+end
+
+# Deploy `supervisord` config:
+remote_file '/etc/supervisor/conf.d/snmp_exporter.conf' do
+  owner  'root'
+  group  'root'
+  mode   '644'
+
+  notifies :restart, 'service[supervisor]'
+end
+
+# Restart the `supervisor`:
+service 'supervisor' do
+  action :nothing
+end

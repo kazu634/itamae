74a3ad149882c536fc490112897f71ccc099b8d0
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2020-10-31 16:53:51 +0900

    Modify role configuration.

--- roles/prometheus.rb
+++ roles/prometheus.rb
@@ -1,3 +1,4 @@
 include_recipe '../cookbooks/prometheus/default.rb'
 include_recipe '../cookbooks/grafana/default.rb'
 include_recipe '../cookbooks/loki/default.rb'
+include_recipe '../cookbooks/vector/syslog_setup.rb'

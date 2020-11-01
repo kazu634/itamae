49b4326aa1768c11cc085304c76389c7f6688a27
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2020-11-01 14:31:00 +0900

    Install `go-mmproxy`.

--- cookbooks/gitea/attributes.rb
+++ cookbooks/gitea/attributes.rb
@@ -9,4 +9,10 @@ node.reverse_merge!({
     'storage' => '/opt/gitea/',
     'location' => '/usr/local/bin/'
   },
+  'go-mmproxy' => {
+    'url' => 'https://github.com/path-network/go-mmproxy/releases/',
+    'bin_url' => 'https://github.com/path-network/go-mmproxy/releases/download/2.0/go-mmproxy-2.0-centos8-x86_64',
+    'storage' => '/opt/go-mmproxy/',
+    'location' => '/usr/local/bin/'
+  },
 })

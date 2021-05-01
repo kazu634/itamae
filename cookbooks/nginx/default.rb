f55682e5c37f1c66fad0fffb434af44a1d073545
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2021-05-01 18:22:31 +0900

    Modify `nginx` deploy settings.

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -4,6 +4,34 @@ include_recipe './attributes.rb'
 # Kernel Parameters:
 include_recipe './kernel.rb'
 
+# Create the necessary directories:
+%w( body fastcgi proxy scgi uwsgi ).each do |d|
+  directory "/var/lib/nginx/#{d}" do
+    owner 'www-data'
+    group 'root'
+    mode '755'
+  end
+end
+
+%w( /etc/nginx/sites-enabled /etc/nginx/stream-enabled ).each do |d|
+  directory d do
+    owner 'root'
+    group 'root'
+    mode  '0755'
+  end
+end
+
+# Deploy the nginx configuration files:
+%w(nginx.conf basic-auth).each do |f|
+  remote_file "/etc/nginx/#{f}" do
+    owner 'root'
+    group 'root'
+    mode '644'
+
+    notifies :reload, 'service[nginx]'
+  end
+end
+
 # Prerequisites for Building nginx:
 include_recipe './webadm.rb'
 

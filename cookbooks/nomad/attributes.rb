935f773bcac4a580775779894b731b6d96b36708
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2022-01-10 15:33:45 +0900

    Deploy `csi` settings.

--- cookbooks/nomad/attributes.rb
+++ cookbooks/nomad/attributes.rb
@@ -5,6 +5,7 @@ node.reverse_merge!({
   'nomad' => {
     'manager' => false,
     'client' => false,
-    'lokiendpoint' => 'loki.service.consul:3100'
+    'lokiendpoint' => 'loki.service.consul:3100',
+    'synology' => '192.168.10.200'
   }
 })
--- cookbooks/nomad/default.rb
+++ cookbooks/nomad/default.rb
@@ -4,6 +4,7 @@ include_recipe './install.rb'
 
 if node['nomad']['manager'] || node['nomad']['client']
   include_recipe './setup.rb'
+  include_recipe './csi.rb'
 
   include_recipe './shared_dir.rb'
 end

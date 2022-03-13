a882f52b3f0ef4ee63dbbfa6742954e4faffc316
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2022-03-13 21:56:59 +0900

    Delete `shared_dir.rb`.

--- cookbooks/nomad/default.rb
+++ cookbooks/nomad/default.rb
@@ -5,6 +5,4 @@ include_recipe './install.rb'
 if node['nomad']['manager'] || node['nomad']['client']
   include_recipe './setup.rb'
   include_recipe './csi.rb'
-
-  include_recipe './shared_dir.rb'
 end

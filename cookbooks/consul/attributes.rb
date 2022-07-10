94489f71d510fe64498b471dd324d9c842171b2d
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2022-07-10 14:31:18 +0900

    Modify `consul.hcl` template to support `Consul Connect`.

--- cookbooks/consul/attributes.rb
+++ cookbooks/consul/attributes.rb
@@ -19,8 +19,10 @@ dns = run_command(cmd).stdout.chomp
 node.reverse_merge!({
   'consul' => {
     'manager' => false,
-    'manager_hosts' => '["192.168.10.101", "192.168.10.251", "192.168.10.252"]',
+    'manager_hosts' => '"192.168.10.101", "192.168.10.251", "192.168.10.252", "192.168.10.253"',
     'ipaddr' => ipaddr,
-    'dns' => dns
+    'dns' => dns,
+    'encrypt' => 's2T3XUTb9MjHYOw8I820O5YkN2G6eJrjLjJRTnEAKoM=',
+    'token' => 'acb7096c-dcda-775a-b52c-b47c96b38d0e'
   }
 })

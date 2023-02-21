da0f63ab5e2306a387f34dc1a8ae6bad96ba4c98
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2023-02-21 23:44:49 +0900

    Deploy `rsyslog` config for `alert manager`.

--- cookbooks/prometheus/alertmanager_webhook_setup.rb
+++ cookbooks/prometheus/alertmanager_webhook_setup.rb
@@ -11,3 +11,15 @@ service 'webhook' do
   action [:enable, :start]
 end
 
+# Deploy `rsyslog` config for `Alert Manager Webhook Logger`:
+remote_file '/etc/rsyslog.d/30-webhook.conf' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+
+  notifies :restart, 'service[rsyslog]'
+end
+
+service 'rsyslog' do
+  action :nothing
+end

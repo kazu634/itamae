a201eea56083b0cf88ad4ff068a54565dc8cfeec
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2023-03-04 15:09:05 +0900

    Deploy `vector` config for `apt`.

--- cookbooks/vector/setup.rb
+++ cookbooks/vector/setup.rb
@@ -7,6 +7,25 @@
   end
 end
 
+# Deploy config for `apt`:
+remote_file '/etc/vector/apt.toml' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+end
+
+remote_file '/etc/systemd/system/vector-apt.service' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+
+  notifies :restart, 'service[vector-apt]'
+end
+
+service 'vector-apt' do
+  action [:enable, :start]
+end
+
 # Stop vector default service:
 service 'vector' do
   action :disable

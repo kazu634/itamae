75755b99f2c6d5a8ed5254fba8574853b69cdd69
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2023-03-04 15:17:27 +0900

    Deploy `vector` config for `/var/log/auth.log`.

--- cookbooks/vector/setup.rb
+++ cookbooks/vector/setup.rb
@@ -12,6 +12,8 @@ remote_file '/etc/vector/apt.toml' do
   owner 'root'
   group 'root'
   mode  '0644'
+
+  notifies :restart, 'service[vector-apt]'
 end
 
 remote_file '/etc/systemd/system/vector-apt.service' do
@@ -25,6 +27,28 @@ end
 service 'vector-apt' do
   action [:enable, :start]
 end
+
+# Deploy config for mointoring `/var/log/auth.log`:
+remote_file '/etc/vector/auth.toml' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+
+  notifies :restart, 'service[vector-auth]'
+end
+
+remote_file '/etc/systemd/system/vector-auth.service' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+
+  notifies :restart, 'service[vector-auth]'
+end
+
+service 'vector-auth' do
+  action [:enable, :start]
+end
+
 
 # Stop vector default service:
 service 'vector' do
